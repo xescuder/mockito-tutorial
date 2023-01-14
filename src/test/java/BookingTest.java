c60390521cc14cc6d69b429f7079c6117dbd86d6
xescuder <xescuder@gmail.com>
2023-01-14 22:52:54 +0100

    :white_check_mark: Added mockito test

--- src/test/java/BookingTest.java
+++ src/test/java/BookingTest.java
@@ -1,40 +1,54 @@
 import booking.*;
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-
+import org.mockito.Spy;
+import org.mockito.junit.jupiter.MockitoExtension;
 import java.time.LocalDate;
 
-public class BookingTest {
+@ExtendWith(MockitoExtension.class)
+class BookingTest {
     @InjectMocks
     private BookingService bookingService;
     @Mock
     private PaymentService paymentServiceMock;
     @Mock
     private RoomService roomServiceMock;
-    @Mock
+    @Spy
     private BookingDAO bookingDAOMock;
     @Mock
     private MailSender mailSenderMock;
 
     @Test
-    void should_CalculateCorrectPrice_When_CorrectInput() {
+    void should_ThrowException_When_NoRoomAvailable() {
         // given
-        PaymentService paymentService = new PaymentService();
-        RoomService roomService = new RoomService();
-        BookingDAO bookingDAO = new BookingDAO();
-        MailSender mailSender = new MailSender();
-
-        bookingService = new BookingService(paymentService, roomService, bookingDAO, mailSender);
         BookingRequest bookingRequest = new BookingRequest("1", LocalDate.of(2022, 02, 18),
-                LocalDate.of(2022, 02, 22), 2, false);
-        double expected = 4 * 2 * 50.0;
+                LocalDate.of(2020, 02, 22), 2, false);
+        when(this.roomServiceMock.findAvailableRoomId(bookingRequest))
+                .thenThrow(BusinessException.class);
+
+        // when then
+        assertThatThrownBy(() -> {
+            bookingService.makeBooking(bookingRequest);
+        }).isInstanceOf(BusinessException.class);
+    }
+
+
+    @Test
+    void should_MakeBooking_When_InputOK() {
+        // given
+        BookingRequest bookingRequest = new BookingRequest("1", LocalDate.of(2022, 02, 18),
+                LocalDate.of(2022, 02, 22), 2, true);
 
         // when
-        double actual = bookingService.calculatePrice(bookingRequest);
+        String bookingId = bookingService.makeBooking(bookingRequest);
 
         // then
-        assertThat(expected).isEqualTo(actual);
+        verify(bookingDAOMock).save(bookingRequest);
+        System.out.println("bookingId=" + bookingId);
     }
 }
